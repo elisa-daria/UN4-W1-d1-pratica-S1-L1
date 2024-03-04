92423569f61aedd0fe0a822612a322b6838d8480
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-03-04 16:45:31 +0100

    Initial commit

---
+++ src/Esercizio1.java
@@ -0,0 +1,14 @@
+public class Esercizio1 {
+    public static void main(String[] args) {
+        int result= multiply(2,3);
+        System.out.println("la moltiplicazione di 2 e 3 é: " +result);
+    }
+    public static int multiply(int num1, int num2){
+        return num1*num2;
+
+    }
+public static void chaining(String[] args){
+
+    System.out.println();
+}
+}
